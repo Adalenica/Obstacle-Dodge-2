ce4da240ca4157fa2a798417ddd05eea60f15f07
Adalenica <aidan@nonatomic.co.uk>
2025-01-08 16:37:46 +0000

    Scoring
    Modified scoring to only work first time player hits an object

--- Assets/Scripts/Scorer.cs
+++ Assets/Scripts/Scorer.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 public class Scorer : MonoBehaviour
 {
     int hits = 0;
-    void OnCollisionEnter(Collision other)   
+    private void OnCollisionEnter(Collision other)   
     {
-        hits = hits +1;
+       if (other.gameObject.tag != "Hit")
+       {
+        hits ++;
         Debug.Log("You've bumped into a thing this many times" + hits);
+       }
     }
 }
