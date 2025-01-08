77e30bf31ede9b66d45196958b806a9b5dd292eb
Adalenica <aidan@nonatomic.co.uk>
2025-01-08 16:14:16 +0000

    Update Dropper.cs
    Created dropper

--- Assets/Scripts/Dropper.cs
+++ Assets/Scripts/Dropper.cs
@@ -2,14 +2,22 @@ using UnityEngine;
 
 public class Dropper : MonoBehaviour
 {
-   
-   
+  [SerializeField] float DropTime = 2f;
+  MeshRenderer myMeshRenderer;
+  Rigidbody myRigidBody;
+   void Start ()
+   {
+        myMeshRenderer = GetComponent<MeshRenderer>();
+        myRigidBody = GetComponent<Rigidbody>();
+        myMeshRenderer.enabled = false;
+        myRigidBody.useGravity = false;
+   }   
     void Update()
     {
-       float DropTime = 3;
         if(Time.time > DropTime)
         {
-            Debug.Log("Lookout Below"!);
+            myMeshRenderer.enabled = true;
+            myRigidBody.useGravity = true;
         }
     }
 }
